fc49041ccb3a27ff49f789c834361448cf0c0a09
Kenan Soylu <ken.soylu@hotmail.com>
2018-08-10 21:24:08 -0500

    sliding gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var maxSlidePx = 0;
+
     var populateGallery = function () {
         var thumbnailURLS = [];
         $.when($.getJSON("src/pictureURLS.json", function (data) {
@@ -14,16 +16,42 @@ $(document).ready(function () {
 
     var generateImages = function (urls) {
         $.each(urls, function (index, picURL) {
-            var img = $("<img />").attr({class: "col col-sm-5 col-md-2 col-lg-2 gallery-img", src: picURL, alt: "Gallery Image"})
+            var img = $("<img />").attr({class: "gallery-img", src: picURL, alt: "Gallery Image"})
             .on('load', function() {
                 if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
                     console.log("Couldn't fetch image: ", picURL);
                 } else {
+                    maxSlidePx += 400;
                     $("#gallery-items").append(img);
                 }
-            });
+            }); 
         });
     }
-
+    
     populateGallery();
+
+    // refactor and refurnish this
+    var slideBy = 400;
+    $("#arrowRight").click(function (e) {
+        var pos = parseInt($("#gallery-items").css('left')); 
+
+        if (pos % 400 == 0){
+            if (pos >= (-maxSlidePx + slideBy)) {
+                $("#gallery-items").css('left', (pos-slideBy)+'px');
+            }   
+        }     
+    });
+
+    $("#arrowLeft").click(function (e) {
+        var pos = parseInt($("#gallery-items").css('left')); 
+
+        if (pos % 400 == 0) {
+            if (pos >= -slideBy) {
+                $("#gallery-items").css('left', '0px');
+            } else if (pos >= (-maxSlidePx + slideBy) && pos < 0) {
+                $("#gallery-items").css('left', (pos+slideBy)+'px');
+            }
+        }
+    });
+
 });
--- js/script.js
+++ js/script.js
@@ -1,61 +1,73 @@
 var rotateBy = 60
-var profilePics = [
-    "https://drive.google.com/uc?id=1IJcqmoeLggICJ85OWT860Oh_xpX6VSUJ", 
-    "https://drive.google.com/uc?id=16peQ6qnSRZvxp1S-8uwtQOhAVtLi_fvo",
-    "https://drive.google.com/uc?id=1absn7C9EBdwuNV2cKfYqmc9_GRJ-LKBu",
-    "https://drive.google.com/uc?id=1ER5oaj0O3t9GTYQdoXzgGC1VhufTv98q",
-    "https://drive.google.com/uc?id=1iviciPu42cHqhZ2Se7kDE28kT4jhfEuN",
-    "https://drive.google.com/uc?id=1n9diTQ8oXklkI9cOuE11XYayrRx22eHF",
-    "https://drive.google.com/uc?id=19vOmmJR6Gk3Efsh3AefZAsB2em9h7NJw"]
-var galleryPics = []
+var profilePics = [];
+
+
+var getRandomPicURL = function (arr, currentURL) {
+    var newPicURL = '';
+    while (newPicURL == '' || newPicURL == currentURL) {
+        newPicURL = arr[Math.floor(Math.random()*arr.length)];
+    }
+    return newPicURL;
+}
+
+var replacePic = function (picID, replaceWith) {
+    // replaces pic with picID with replaceWith
+    // replaceWith is html, picID -> #foo
+    if (picID.charAt(0) == "#" || picID.charAt(0) == ".") {
+        $(picID).replaceWith(replaceWith);
+    } else {
+        $("#"+picID).replaceWith(replaceWith);
+    }
+}
+
+var setNewPic = function (img) { 
+    // replaces the img with a new one
+
+    var currentPicURL = img.attr('src');
+    var newPicURL = '';
+
+    // had to place it in a function since the async calls didn't work out
+    var replace = function () { 
+        var img = $("<img />").attr({id: "profileIMG", src: newPicURL, style: "max-width: 100%; max-height: 100%;", alt: "Profile Picture"})
+        .on('load', function() {
+            if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+                console.log("Couldn't fetch image: ", newPicURL);
+                var defaultPic = $("<img />").attr({id: "profileIMG", src: "../src/profile.jpg", style: "max-width: 100%; max-height: 100%;", alt: "Profile Picture"}) 
+                replacePic(img.attr('id'), defaultPic);
+            } else {
+                replacePic(img.attr('id'), img);
+            }
+        });
+    }
+
+    if (profilePics.length == 0) {
+        // populate the array 
+        $.when($.getJSON("src/pictureURLS.json", function (data) {
+            profilePics = data.profilePics;
+        })).done(function () {
+            newPicURL = getRandomPicURL(profilePics, currentPicURL);
+            replace();
+        });
+    } else {
+        newPicURL = getRandomPicURL(profilePics, currentPicURL);
+        replace();
+    }
+}
+
+var getMouseRelativePosition = function (elem, e) {
+    // get mouse position relative to the elem
+    
+    var parentOffset = elem.offset(); 
+    var relX = e.pageX - parentOffset.left;
+
+    return relX;
+}
+
 
 $(document).ready(function () {
 
-    var replacePic = function (picID, replaceWith) {
-        // replaces pic with picID with replaceWith
-        // replaceWith is html, picID -> #foo
-        if (picID.charAt(0) == "#") {
-            $(picID).replaceWith(replaceWith);
-        }else {
-            $("#"+picID).replaceWith(replaceWith);
-        }
-    }
-
-    var setNewPic = function (img) { 
-        // returns new image element with image attached
-
-        // default pic 
-        var newPicURL = '';
-
-        var currentPicURL = img.attr('src');
-
-        while (newPicURL == '' || newPicURL == currentPicURL) {
-            newPicURL = profilePics[Math.floor(Math.random()*profilePics.length)];
-        }
-        
-        var img = $("<img />").attr({id: "profileIMG", src: newPicURL, style: "max-width: 100%; max-height: 100%;", alt: "Profile Picture"})
-            .on('load', function() {
-                if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-                    console.log("Couldn't fetch image: ", newPicURL);
-                    var defaultPic = $("<img />").attr({id: "profileIMG", src: "../src/profile.jpg", style: "max-width: 100%; max-height: 100%;", alt: "Profile Picture"}) 
-                    replacePic(img.attr('id'), defaultPic);
-                } else {
-                    replacePic(img.attr('id'), img);
-                }
-            });
-    }
-
     // set initial photo
     setNewPic($("#profileIMG"));
-
-    var getMouseRelativePosition = function (elem, e) {
-        // get mouse position relative to the elem
-        
-        var parentOffset = elem.offset(); 
-        var relX = e.pageX - parentOffset.left;
-
-        return relX;
-    }
 
     $("#profilePicContainer").mousemove(function (e) {
         // rotate the picture
